850080a7cf9692a75bdacf72eaecedde740becfd
WilliamRausch <nolanrausch@Gmail.com>
2017-08-17 17:41:46 -0400

    technically done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+let errors;
+let messages;
  let user = {
  	username: "Will",
  	password: "password"
@@ -15,7 +17,7 @@ router.get("/", function (req, res){
 }
 });
 router.get("/login", function(req, res){
-	res.render("login")
+	res.render("login", messages)
 
 });
 router.post("/",function(req, res){
@@ -24,8 +26,21 @@ router.post("/",function(req, res){
 	req.checkBody("username", "name too long").isLength({max: 25});
 	req.checkBody("password", "password too short").isLength({min: 8});
 	req.checkBody("username", "name too long").isLength({min: 8});
+	req.checkBody("password", "no special characters").isAlpha();
 	
+errors = req.getValidationResult();
+messages = [];
+errors.then(function(result) {
+    result.array().forEach(function(error) {
+      messages.push(error.msg);
+    });
 
+  console.log("hello");
+    console.log(messages);
+    if(messages.length > 0){
+    	res.redirect("/login");
+
+    }
 obj = {
     username: req.body.username,
     password: req.body.password
@@ -39,5 +54,6 @@ obj = {
   } else {
     res.redirect("/login");
   }
+});
 });
 module.exports = router;
